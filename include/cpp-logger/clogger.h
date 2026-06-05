89ea679e91e2372603b3b608738da3bc7693069c
Jae-Seung Yeom <yeom2@llnl.gov>
2026-06-04 17:30:15 -0700

    Fix macro name conflict

--- include/cpp-logger/clogger.h
+++ include/cpp-logger/clogger.h
@@ -1,12 +1,12 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#define CPP_LOGGER_PRINT 1
-#define CPP_LOGGER_ERROR 2
-#define CPP_LOGGER_WARN 3
-#define CPP_LOGGER_INFO 4
-#define CPP_LOGGER_DEBUG 5
-#define CPP_LOGGER_TRACE 6
+#define CPP_LOGGER_C_PRINT 1
+#define CPP_LOGGER_C_ERROR 2
+#define CPP_LOGGER_C_WARN 3
+#define CPP_LOGGER_C_INFO 4
+#define CPP_LOGGER_C_DEBUG 5
+#define CPP_LOGGER_C_TRACE 6
 #include <stdio.h>
 extern void cpp_logger_clog(const int logger_level, const char* name, const char* string,
                  ...);
--- include/cpp-logger/logger.h
+++ include/cpp-logger/logger.h
@@ -12,13 +12,13 @@
 
 namespace cpplogger {
 enum LoggerType {
-  NO_LOG = 0,
-  LOG_PRINT = 1,
-  LOG_ERROR = 2,
-  LOG_WARN = 3,
-  LOG_INFO = 4,
-  LOG_DEBUG = 5,
-  LOG_TRACE = 6,
+  CPP_LOGGER_NONE = 0,
+  CPP_LOGGER_PRINT = 1,
+  CPP_LOGGER_ERROR = 2,
+  CPP_LOGGER_WARN = 3,
+  CPP_LOGGER_INFO = 4,
+  CPP_LOGGER_DEBUG = 5,
+  CPP_LOGGER_TRACE = 6,
 };
 
 class Logger {
@@ -31,7 +31,7 @@ class Logger {
   LoggerType level;
 
   explicit Logger(std::string app_name, FILE *file = stdout)
-      : _app_name(app_name), _file(file), level(LoggerType::LOG_ERROR) {}
+      : _app_name(app_name), _file(file), level(LoggerType::CPP_LOGGER_ERROR) {}
 
   static std::shared_ptr<Logger> Instance(std::string app_name = "LOGGER", FILE *file = stdout) {
     auto iter = instance_map.find(app_name);
@@ -52,43 +52,43 @@ class Logger {
     int resu = vsprintf(buffer, string, args);
     (void)resu;
     switch (type) {
-      case LoggerType::LOG_PRINT: {
-        if (level >= LoggerType::LOG_PRINT) {
+      case LoggerType::CPP_LOGGER_PRINT: {
+        if (level >= LoggerType::CPP_LOGGER_PRINT) {
           fprintf(_file, "[%s PRINT]: %s\n", _app_name.c_str(), buffer);
           fflush(_file);
         }
         break;
       }
-      case LoggerType::LOG_TRACE: {
-        if (level >= LoggerType::LOG_TRACE) {
+      case LoggerType::CPP_LOGGER_TRACE: {
+        if (level >= LoggerType::CPP_LOGGER_TRACE) {
           fprintf(_file, "[%s TRACE]: %s\n", _app_name.c_str(), buffer);
           fflush(_file);
         }
         break;
       }
-      case LoggerType::LOG_DEBUG: {
-        if (level >= LoggerType::LOG_DEBUG) {
+      case LoggerType::CPP_LOGGER_DEBUG: {
+        if (level >= LoggerType::CPP_LOGGER_DEBUG) {
           fprintf(_file, "[%s DEBUG]: %s\n", _app_name.c_str(), buffer);
           fflush(_file);
         }
         break;
       }
-      case LoggerType::LOG_INFO: {
-        if (level >= LoggerType::LOG_INFO) {
+      case LoggerType::CPP_LOGGER_INFO: {
+        if (level >= LoggerType::CPP_LOGGER_INFO) {
           fprintf(_file, "[%s INFO]: %s\n", _app_name.c_str(), buffer);
           fflush(_file);
         }
         break;
       }
-      case LoggerType::LOG_WARN: {
-        if (level >= LoggerType::LOG_WARN) {
+      case LoggerType::CPP_LOGGER_WARN: {
+        if (level >= LoggerType::CPP_LOGGER_WARN) {
           fprintf(_file, "[%s WARN]: %s\n", _app_name.c_str(), buffer);
           fflush(_file);
         }
         break;
       }
-      case LoggerType::LOG_ERROR: {
-        if (level >= LoggerType::LOG_ERROR) {
+      case LoggerType::CPP_LOGGER_ERROR: {
+        if (level >= LoggerType::CPP_LOGGER_ERROR) {
           fprintf(_file, "[%s ERROR]: %s\n", _app_name.c_str(), buffer);
           fflush(_file);
         }
